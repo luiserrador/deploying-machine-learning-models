4647d77724ede66be07ae0c412a97eadfc0218a2
Luis Serrador <25669570+luiserrador@users.noreply.github.com>
2024-01-10 12:27:37 +0000

    adding my updates

---
+++ assignment-section-05-mine/classification_model/processing/validation.py
@@ -0,0 +1,45 @@
+from typing import Tuple, Optional, List, Union
+
+import numpy as np
+import pandas as pd
+from pydantic import BaseModel, ValidationError
+
+from classification_model.config.core import config
+from classification_model.processing.data_manager import pre_pipeline_preparation
+
+
+def validate_inputs(*, input_data: pd.DataFrame) -> Tuple[pd.DataFrame, Optional[dict]]:
+    """Check model inputs for unprocessable values."""
+
+    pre_processed = pre_pipeline_preparation(dataframe=input_data)
+    validated_data = pre_processed[config.model_config.features].copy()
+    errors = None
+
+    try:
+        # replace numpy nans so that pydantic can validate
+        MultipleHouseDataInputs(
+            inputs=validated_data.replace({np.nan: None}).to_dict(orient="records")
+        )
+    except ValidationError as error:
+        errors = error.json()
+
+    return validated_data, errors
+
+
+class HouseDataInputSchema(BaseModel):
+    pclass: Optional[int]
+    name: Optional[str]
+    sex: Optional[str]
+    age: Optional[int]
+    sibsp: Optional[int]
+    parch: Optional[int]
+    ticket: Optional[int]
+    fare: Optional[float]
+    cabin: Optional[str]
+    embarked: Optional[str]
+    boat: Optional[Union[int, str]]
+    body: Optional[Union[int, str]]
+
+
+class MultipleHouseDataInputs(BaseModel):
+    inputs: List[HouseDataInputSchema]
