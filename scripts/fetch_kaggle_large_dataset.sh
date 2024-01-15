3a0adf4c724ad5696e43b9c0dd8836e0878e1c67
Luis Serrador <25669570+luiserrador@users.noreply.github.com>
2024-01-15 18:50:29 +0000

    add section13 on config

--- scripts/fetch_kaggle_large_dataset.sh
+++ scripts/fetch_kaggle_large_dataset.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
-TRAINING_DATA_URL="vbookshelf/v2-plant-seedlings-dataset"
-NOW=$(date)
+# TRAINING_DATA_URL="vbookshelf/v2-plant-seedlings-dataset"
+# NOW=$(date)
 
-kaggle datasets download -d $TRAINING_DATA_URL -p packages/neural_network_model/neural_network_model/datasets/ && \
-unzip packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset.zip -d packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset && \
-echo $TRAINING_DATA_URL 'retrieved on:' $NOW > packages/neural_network_model/neural_network_model/datasets/training_data_reference.txt #&& \
+# kaggle datasets download -d $TRAINING_DATA_URL -p packages/neural_network_model/neural_network_model/datasets/ && \
+# unzip packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset.zip -d packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset && \
+# echo $TRAINING_DATA_URL 'retrieved on:' $NOW > packages/neural_network_model/neural_network_model/datasets/training_data_reference.txt #&& \
 # mkdir -p "./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/Shepherds Purse"  && \
 # mv -v "./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/ShepherdGÇÖs Purse/"* "./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/Shepherds Purse"
 # rm -rf "./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/ShepherdGÇÖs Purse"
 # rm -rf "./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/nonsegmentedv2"
+ 
+TRAINING_DATA_URL="vbookshelf/v2-plant-seedlings-dataset"
+NOW=$(date)
+ 
+kaggle datasets download -d $TRAINING_DATA_URL -p packages/neural_network_model/neural_network_model/datasets/ && \
+unzip packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset.zip -d packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset && \
+echo $TRAINING_DATA_URL 'retrieved on:' $NOW > packages/neural_network_model/neural_network_model/datasets/training_data_reference.txt && \
+cd packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset && \
+mv *Shepherd* "Shepherds Purse"
